685e697ee51792a5888a8288c2b055bed8fbff8f
iamahmadmhd <ahmad.mohaddeszadeh@gmail.com>
2025-01-20 15:48:18 +0100

    Updated app for react router and new pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 import './App.css';
-import About from './components/about';
 import Footer from './components/footer';
 import Header from './components/header';
-import HeroSection from './components/hero-section';
-import Highlights from './components/highlights';
-import Testimonials from './components/testimonials';
+import Home from "./pages/home";
+import Reservations from "./pages/reservations";
+import ReservationConfirmation from "./pages/reservation-confirmation";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <main>
-        <HeroSection />
-        <Highlights />
-        <Testimonials />
-        <About />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="reservations" element={<Reservations />} />
+        <Route path="reservation-confirmation" element={<ReservationConfirmation />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
