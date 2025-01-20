a09d4c81885a401ed8a31dcc154d01237f1f135f
iamahmadmhd <ahmad.mohaddeszadeh@gmail.com>
2025-01-20 15:42:29 +0100

    Adjusted button

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,11 +1,22 @@
-const Button = ({ children, variant, ...props }) => {
-    const className = "py-3 px-6 font-semibold rounded-2xl";
-    if (variant === "primary") {
-        return <button className={`${className} bg-yellow text-black`} {...props}>{children}</button>;
-    } else if (variant === "secondary") {
-        return <button className={`${className} bg-black text-white`} {...props}>{children}</button>;
-    }
-    return null;
+const Button = ({ children, variant, type, ...props }) => {
+    const baseStyles = "py-3 px-6 font-semibold rounded-2xl transition-all";
+    const variantStyles = {
+        primary: "bg-yellow text-black border-4 border-transparent hover:text-yellow hover:bg-transparent hover:border-yellow",
+        secondary: "bg-green text-white border-4 border-transparent hover:text-green hover:bg-transparent hover:border-green",
+        link: "group flex items-center space-x-2 text-green",
+    };
+
+    const selectedVariant = variantStyles[variant];
+
+    return selectedVariant ? (
+        <button type={type} className={`${baseStyles} ${selectedVariant}`} {...props}>
+            {children}
+        </button>
+    ) : (
+        <span className="text-red-500 font-semibold">
+            Error: Invalid variant "{variant}" provided to Button.
+        </span>
+    );
 };
 
 export default Button;
