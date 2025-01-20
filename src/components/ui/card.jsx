84c9b3cab61d4c84a347db30699c3928c78ad05b
iamahmadmhd <ahmad.mohaddeszadeh@gmail.com>
2025-01-20 15:43:24 +0100

    Adjusted card component

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,14 +1,12 @@
-const Card = ({ image, title, description, link }) => {
-    return (
-        <div className="bg-gray shadow-lg rounded-2xl overflow-hidden">
-            <img src={image} alt={title} className="w-full mb-4 aspect-4/3 object-cover" />
-            <div className="p-6 flex flex-col gap-4">
-                <h3 className="text-2xl font-semibold">{title}</h3>
-                <p className="">{description}</p>
-                <a href={link.href} className="mt-auto font-semibold">{link.label}</a>
-            </div>
+const Card = ({ image, title, description, link: { href, label } }) => (
+    <article className="bg-gray shadow-lg rounded-2xl overflow-hidden">
+        <img src={image} alt={title} className="w-full aspect-4/3 object-cover" />
+        <div className="p-6 flex flex-col gap-4">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <p>{description}</p>
+            <a href={href} className="font-semibold">{label}</a>
         </div>
-    );
-};
+    </article>
+);
 
 export default Card;
