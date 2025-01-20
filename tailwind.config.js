2e86af09a1d8ee60792a748abc6569ad981f0c93
iamahmadmhd <ahmad.mohaddeszadeh@gmail.com>
2025-01-20 15:41:56 +0100

    Added react router, changed tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    colors: {
-      "yellow": "#f4ce14",
-      "green": "#495e57",
-      "white": "#ffffff",
-      "black": "#000000",
-      "gray": "#EDEFEE",
-    },
+
     container: {
       center: true,
       padding: "1.5rem",
       screens: {
-        sm: "100%",
-        md: "100%",
+        sm: "640px",
+        md: "768px",
         lg: "1024px",
         xl: "1280px",
       },
@@ -27,11 +21,21 @@ module.exports = {
       aspectRatio: {
         '4/3': '4 / 3',
       },
+      colors: {
+        "yellow": "#f4ce14",
+        "green": "#495e57",
+        "white": "#ffffff",
+        "black": "#000000",
+        "gray": "#EDEFEE",
+        "transparent": "transparent",
+      },
       screens: {
         'xs': '480px'
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
